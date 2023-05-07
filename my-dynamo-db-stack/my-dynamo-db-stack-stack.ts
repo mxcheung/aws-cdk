bdff0ce9ecb0e5c701abea59f7b4bdc2c9fc9120
mxcheung <mxcheung@users.noreply.github.com>
2023-05-07 14:36:17 +1000

    Create my-dynamo-db-stack-stack.ts

---
+++ my-dynamo-db-stack/my-dynamo-db-stack-stack.ts
@@ -0,0 +1,26 @@
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+
+export class MyDynamoDbStackStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'MyDynamoDbStackQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+    
+    //define dynamodb table
+    const dynamodb_table = new dynamodb.Table(this, "Table", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY
+      }
+    )
+    
+  }
+}
