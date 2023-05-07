078ed2ccd08352ca43a01d9085583ca130764008
mxcheung <mxcheung@users.noreply.github.com>
2023-05-07 14:23:56 +1000

    Create first-cdk-app-stack.ts

---
+++ first-cdk-app/first-cdk-app-stack.ts
@@ -0,0 +1,17 @@
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export class MyDynamoDBStack extends Stack {
+ constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+
+    //define dynamodb table
+    const dynamodb_table = new dynamodb.Table(this, "Table", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY
+      }
+    )
+  }
+}
